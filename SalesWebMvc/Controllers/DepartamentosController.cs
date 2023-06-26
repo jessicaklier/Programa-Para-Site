2df9e872e443cd58f061fcd799155b3215a47945
jessicaklier <jhessicadesouza12@hotmail.com>
2023-06-26 13:48:26 -0300

    Primeiro Modelo Controlador, Visualizador de Departameto

---
+++ SalesWebMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Indice = 1, Nome = "Eletronicos" });
+            list.Add(new Departamento { Indice = 2, Nome = "Moda" });
+
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Departamento.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Departamento
+    {
+        public int Indice { get; set; }
+        public string Nome { get; set; }
+    }
+}
