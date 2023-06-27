191a968c58ed25a5eb2f0bfc67f1288b1bd9c754
jessicaklier <jhessicadesouza12@hotmail.com>
2023-06-27 15:48:38 -0300

    Serviço de vendedor e toda localização básica

--- SalesWebMvc/Controllers/VendedoresController.cs
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -3,14 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
+        private readonly VendedorService _vendedorService;
+        public VendedoresController(VendedorService vendedorService)
+        {
+            _vendedorService = vendedorService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _vendedorService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMvc/Services/VendedorService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Services;
+using SalesWebMvc.Models;
+using SalesWebMvc.Data;
+
+namespace SalesWebMvc.Services
+{
+    public class VendedorService
+    {
+        private readonly SalesWebMvcContext _context;
+        public VendedorService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
