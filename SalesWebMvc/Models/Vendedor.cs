e79234eefe8a05c7af3b5e3d843a227e0074cade
jessicaklier <jhessicadesouza12@hotmail.com>
2023-06-29 16:54:40 -0300

    Formatação de localidade, número e data do aplicativo

--- SalesWebMvc/Models/Vendedor.cs
+++ SalesWebMvc/Models/Vendedor.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
@@ -9,8 +9,14 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Data De Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataDeNascimento { get; set; }
+        [Display(Name = "Salario Base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SalarioBase { get; set; }
         public Departamento Departamento { get; set; }
         public int DepartamentoId { get; set; }
