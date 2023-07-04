e8fead08548ac63996948c71f308fa8b661f013b
jessicaklier <jhessicadesouza12@hotmail.com>
2023-07-04 14:59:06 -0300

    Preparando modos de exibição de navegação de pesquisa de vendas

---
+++ SalesWebMvc/Controllers/RegistroDeVendasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class RegistroDeVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PesquisaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult PesquisaDeAgrupamento()
+        {
+            return View();
+        }
+    }
+}
