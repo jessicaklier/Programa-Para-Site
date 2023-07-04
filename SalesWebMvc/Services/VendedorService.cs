a2a3d4d227a81cf3979f20a923d46b500a33f2ee
jessicaklier <jhessicadesouza12@hotmail.com>
2023-07-04 14:31:17 -0300

    Tratamento de exceções para exclusão(integridade referencial)

--- SalesWebMvc/Services/VendedorService.cs
+++ SalesWebMvc/Services/VendedorService.cs
@@ -35,9 +35,16 @@ namespace SalesWebMvc.Services
         }
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Vendedor.FindAsync(id);
-            _context.Vendedor.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Vendedor.FindAsync(id);
+                _context.Vendedor.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Não pode deletar o vendedor pois ele tem vendas.");
+            }
         }
         public async Task UpdateAsync(Vendedor obj)
         {
